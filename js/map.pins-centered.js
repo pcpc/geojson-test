b053e4871af8fa917faf3d843ab62ccbf52f4373
bradfordbradford <bradford@wearetelegraph.com>
2013-11-20 20:27:03 -0600

    Updated method for pulling in JSON URL so that Map centers on clicked Pin. Hover is optional.

--- js/map.pins-centered.js
+++ js/map.pins-centered.js
@@ -3,8 +3,11 @@
 // *
 
 
-// Set Map ID+View
-var map = L.mapbox.map('map', 'pcpc.map-qr8fr1li').setView([32.81942355912533,-96.8020337820053], 11);
+// Set Map ID+View+Provide Retina Alternate Map
+var map = L.mapbox.map('map', 'pcpc.map-qr8fr1li').setView([32.81942355912533,-96.8020337820053], 11)
+.addLayer(L.mapbox.tileLayer('pcpc.map-qr8fr1li', {
+    detectRetina: true
+}));
 
 // Path to GeoJSON Pins
 var jsonPinsURL = 'test.geojson';
